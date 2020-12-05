5040335270f5ecf4e9ad11741534ab9912d634ac
vikycoco28 <74967974+vikycoco28@users.noreply.github.com>
2020-12-05 14:15:52 +0100

    Almost working undo, minor formatting changes
    Undo and redo work, but it takes 2 presses for undo to show any changes.
    Moreover it seems like the last undo/redo doesn't render until a mouse event updates the appstate.

--- Figures_and_Rendering.rkt
+++ Figures_and_Rendering.rkt
@@ -9,43 +9,34 @@
 ;; Data types
 
 ; a Tool is a structure (make-tool type size color mode x1 y1 x2 y2 status)
-; where   type : String
-;         size : Number
-;        color : Color    ; (in 2htdp/image there's already a struct for color, we could use that)
-;         mode : String or Number
-;                represents the mode for the figures ("outline", "solid", ...)        
-;                (it can also be a number from 0 to 255 that indicates its transparency, with 0 being fully transparent and 255 being "solid")
-;       x1, y1 : Number
-;                represents the coordinate of the starting point of the current figure/tool
-;       x2, y2 : Number
-;                represents the coordinate of the ending point of the current figure/tool
-(define-struct tool [type size color mode x1 y1 x2 y2]) 
+; where type : String
+;       size : Number
+;       color : Color ;(in 2htdp/image there's already a struct for color, we could use that)
+;       mode : String or Number
+;              represents the mode for the figures ("outline", "solid", ...)        
+;              (it can also be a number from 0 to 255 that indicates its transparency, with 0 being fully transparent and 255 being "solid")
+;       x1, y1, x2, y2 : Number
+;                        represent the coordinates of the current figure/tool
+(define-struct tool [type size color mode x1 y1 x2 y2])
 
-; a List<Image> is on of:
+; a List<Image> is one of:
 ; - '()
-; - (cons Line List<Image>)
-; interpretation: a list of images
+; - (cons Image List<Image>)
+; interpretation: a list of canvases
 
 ; an AppState is a structure (make-appstate canvas pre post tool quit)
 ; where canvas : Image
-;          pre : List<Image>
-;         post : List<Image>
-;         tool : Tool
-;         quit : Boolean
+;       pre, post : List<Image>
+;       tool : Tool
+;       quit : Boolean
 ; interpretation: it represents the current state of the app
 (define-struct appstate [canvas pre post tool quit])
+
 ; Examples of data
 (define IS (make-appstate EMPTY-CANVAS
                           '()
                           '()
-                          (make-tool "square"
-                                     1
-                                     "black"
-                                     "outline"
-                                     0
-                                     0
-                                     0
-                                     0)
+                          (make-tool "square" 1 "black" "outline" 0 0 0 0)
                           #false))
  
 ; ==================================================================================================
@@ -56,11 +47,16 @@
 
 ;; Code
 (define (render app)
-  (cond
-    [(string=? "square" (tool-type (appstate-tool app))) (draw-square app)]
-    ;[(string?= "rectangle" (tool-type (appstate-tool app))) (draw-rectangle)]
-    
-    [else (appstate-canvas app)]))
+  (local ((define type (tool-type (appstate-tool app))))
+    (overlay
+     (cond
+      [(string=? type "square") (draw-square app)]
+     ;[(string=? type "rectangle") (draw-rectangle)]
+      [else (appstate-canvas app)])
+     (appstate-canvas app))))
+     
+
+; ==================================================================================================
 
 ;; Input/Output
 ; draw-square : AppState -> Image
@@ -70,6 +66,8 @@
 (define (draw-square app)
   (local
     ((define S (square-size (appstate-tool app)))
+     (define MODE (tool-mode (appstate-tool app)))
+     (define COLOR (tool-color (appstate-tool app)))
      (define X1 (tool-x1 (appstate-tool app)))
      (define X2 (tool-x2 (appstate-tool app)))
      (define Y1 (tool-y1 (appstate-tool app)))
@@ -77,25 +75,25 @@
   (cond
     [(and (> X1 X2) (> Y1 Y2))
      (place-image
-      (square S (tool-mode (appstate-tool app)) (tool-color (appstate-tool app)))
+      (square S MODE COLOR)
       (- X1 (/ (integer-sqrt (* 2 (* S S))) 2)) 
       (- Y1 (/ (integer-sqrt (* 2 (* S S))) 2))
       (appstate-canvas app))]
     [(and (< X1 X2) (< Y1 Y2))
      (place-image
-      (square S (tool-mode (appstate-tool app)) (tool-color (appstate-tool app)))
+      (square S MODE COLOR)
       (+ X1 (/ (integer-sqrt (* 2 (* S S))) 2)) 
       (+ Y1 (/ (integer-sqrt (* 2 (* S S))) 2))
       (appstate-canvas app))]
     [(and (> X1 X2) (< Y1 Y2))
      (place-image
-      (square S (tool-mode (appstate-tool app)) (tool-color (appstate-tool app)))
+      (square S MODE COLOR)
       (- X1 (/ (integer-sqrt (* 2 (* S S))) 2)) 
       (+ Y1 (/ (integer-sqrt (* 2 (* S S))) 2))
       (appstate-canvas app))]
     [(and (< X1 X2) (> Y1 Y2))
      (place-image
-      (square S (tool-mode (appstate-tool app)) (tool-color (appstate-tool app)))
+      (square S MODE COLOR)
       (+ X1 (/ (integer-sqrt (* 2 (* S S))) 2)) 
       (- Y1 (/ (integer-sqrt (* 2 (* S S))) 2))
       (appstate-canvas app))]
@@ -107,10 +105,15 @@
 
 ;: Code
 (define (square-size t)
-  (cond
-    [(<= (abs (- (tool-x2 t) (tool-x1 t))) (abs (- (tool-y2 t) (tool-y1 t))))
-     (abs (- (tool-x2 t) (tool-x1 t)))]
-    [else (abs (- (tool-y2 t) (tool-y1 t)))]))
+  (local ((define X1 (tool-x1 t))
+          (define Y1 (tool-y1 t))
+          (define X2 (tool-x2 t))
+          (define Y2 (tool-y2 t)))
+    (cond
+      [(<= (abs (- X2 X1)) (abs (- Y2 Y1)))
+       (abs (- X2 X1))]
+      [else
+       (abs (- Y2 Y1))])))
 
 ;; Input/Output
 ; add-figure-to-canvas : AppState -> AppState
@@ -118,11 +121,13 @@
 
 ;; Code
 (define (add-figure-to-canvas app)
-  (cond
-    [(string=? "square" (tool-type (appstate-tool app))) (add-square-to-canvas app)]
-    ;[(string?= "rectangle" (tool-type (appstate-tool app))) (add-rectangle-to-canvas app)]
+  (local ((define type (tool-type (appstate-tool app))))
+    (cond
+      [(string=? type "square") (add-square-to-canvas app)]
+     ;[(string=? type "line") (add-line-to-canvas app)]
+     ;[(string=? type "rectangle") (add-rectangle-to-canvas app)]
     
-    [else app]))
+    [else app])))
 
 ;; Input/Output
 ; add-square-to-canvas : AppState -> AppState
@@ -137,33 +142,9 @@
      (define Y1 (tool-y1 (appstate-tool app)))
      (define Y2 (tool-y2 (appstate-tool app))))  
   (make-appstate
-   (cond
-    [(and (>= X1 X2) (>= Y1 Y2))
-     (place-image
-      (square S (tool-mode (appstate-tool app)) (tool-color (appstate-tool app)))
-      (- X1 (/ (integer-sqrt (* 2 (* S S))) 2)) 
-      (- Y1 (/ (integer-sqrt (* 2 (* S S))) 2))
-      (appstate-canvas app))]
-    [(and (< X1 X2) (< Y1 Y2))
-     (place-image
-      (square S (tool-mode (appstate-tool app)) (tool-color (appstate-tool app)))
-      (+ X1 (/ (integer-sqrt (* 2 (* S S))) 2)) 
-      (+ Y1 (/ (integer-sqrt (* 2 (* S S))) 2))
-      (appstate-canvas app))]
-    [(and (> X1 X2) (< Y1 Y2))
-     (place-image
-      (square S (tool-mode (appstate-tool app)) (tool-color (appstate-tool app)))
-      (- X1 (/ (integer-sqrt (* 2 (* S S))) 2)) 
-      (+ Y1 (/ (integer-sqrt (* 2 (* S S))) 2))
-      (appstate-canvas app))]
-    [(and (< X1 X2) (> Y1 Y2))
-     (place-image
-      (square S (tool-mode (appstate-tool app)) (tool-color (appstate-tool app)))
-      (+ X1 (/ (integer-sqrt (* 2 (* S S))) 2)) 
-      (- Y1 (/ (integer-sqrt (* 2 (* S S))) 2))
-      (appstate-canvas app))]
-    [else (appstate-canvas app)])
-   (cons (appstate-canvas app) (appstate-pre app))
+   (draw-square app)
+   (cons (appstate-canvas app)
+         (appstate-pre app))
    '()
    (appstate-tool app)
    (appstate-quit app))))
@@ -188,7 +169,37 @@
                                 new-x
                                 new-y)
                      (appstate-quit app))))
-                                
+
+; ==================================================================================================
+; Undo + Render Functions
+
+; Undo ------------------------------------------------------------------------------
+
+(define (undo a)
+  (cond [(empty? (appstate-pre a)) a]
+        [else
+         (make-appstate
+          (first (appstate-pre a))
+          (rest (appstate-pre a))
+          (cons (appstate-canvas a)
+                (appstate-post a))
+          (appstate-tool a)
+          #false)]))
+
+; Redo ------------------------------------------------------------------------------
+
+(define (redo a)
+  (cond [(empty? (appstate-post a)) a]
+        [else
+         (make-appstate
+          (first (appstate-post a))
+          (cons (appstate-canvas a)
+                (appstate-pre a))
+          (rest (appstate-post a))
+          (appstate-tool a)
+          #false)]))
+
+; ==================================================================================================
 
 ;; Input/output
 ; handle-mouse : MouseEvent AppState -> AppState
@@ -203,20 +214,60 @@
            (string=? type "rectangle")
            (string=? type "circle")
            (string=? type "ellipse"))
-       (cond [(string=? me "button-down") (make-appstate (appstate-canvas a)
-                                                         (appstate-pre a)
-                                                         (appstate-post a)
-                                                         (make-tool (tool-type (appstate-tool a))
-                                                                    (tool-size (appstate-tool a))
-                                                                    (tool-color (appstate-tool a))
-                                                                    (tool-mode (appstate-tool a))
-                                                                    x y x y)
-                                                         (appstate-quit a))]
-             [(string=? me "drag") (move-end a x y)]
-             [(string=? me "button-up") (add-figure-to-canvas a)]
+       (cond [(string=? me "button-down")
+              (make-appstate (appstate-canvas a)
+                             (appstate-pre a)
+                             (appstate-post a)
+                             (make-tool (tool-type (appstate-tool a))
+                                        (tool-size (appstate-tool a))
+                                        (tool-color (appstate-tool a))
+                                        (tool-mode (appstate-tool a))
+                                        x y x y)
+                             (appstate-quit a))]
+             [(string=? me "drag")
+              (move-end a x y)]
+             [(string=? me "button-up")
+              (add-figure-to-canvas a)]
              [else a])]
 ;     []                                                    ; for other tools
       [else a])))
+
+; ====================================================================================
+; Key Handler
+
+; AUXILIARY FUNCTION
+; set-tool: AppState Tool -> AppState
+; change tool type to corresponding input
+; (define (set-tool a t) a)
+
+(define (set-tool-type a t)
+  (make-appstate
+   (appstate-canvas a)
+   (appstate-pre a)
+   (appstate-post a)
+   (make-tool t ; set tool-type to "t" input
+              (tool-size (appstate-tool a))
+              (tool-color (appstate-tool a))
+              (tool-mode (appstate-tool a))
+              (tool-x1 (appstate-tool a))
+              (tool-y1 (appstate-tool a))
+              (tool-x2 (appstate-tool a))
+              (tool-y2 (appstate-tool a)))
+   (appstate-quit a)))
+
+; handle-key: AppState KeyEvent -> AppState
+; Takes a key event and calls the function to perform the corresponding change
+; to the current appstate/canvas
+; (define (handle-key a k) a)
+
+(define (handle-key a k)
+  (cond [(string=? k "u") (undo a)]
+        [(string=? k "r") (redo a)]
+       ;[(string=? k "l") (set-tool-type a "line")] ; behaves weirdly with the rendering function
+        [(string=? k "s") (set-tool-type a "square")]
+        [else a]))
+
+; ==================================================================================================
 
 ;; Input/Output
 ; quit? : AppState -> Boolean
@@ -225,6 +276,8 @@
 
 ;; Code
 (define (quit? app) (appstate-quit app))
+
+; ==================================================================================================
 
 ;; Input/Output
 ; draw-app : AppState -> AppState
@@ -235,6 +288,5 @@
   (big-bang is
     [to-draw render]
     [on-mouse handle-mouse]
-    ;[on-key handle-key]
+    [on-key handle-key]
     [stop-when quit?]))
-                                
